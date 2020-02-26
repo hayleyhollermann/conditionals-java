0a81814c84a4b210b7e6e22915e261975f9077e7
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 14:12:25 -0600

    if-then conditional practice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    
+	    boolean isAlien = false;
+	    if (isAlien) {
+            System.out.println("IT IS AN ALIEN");
+        } else if(!isAlien) {
+            System.out.println("it is not an alien!");
+        }
     }
 }
