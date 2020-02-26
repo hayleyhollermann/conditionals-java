47a810edfa700eeaa3995cb1a5fe7219b858c4d3
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 14:18:03 -0600

    practicing conditionals

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
 	    boolean isAlien = false;
 	    if (isAlien) {
             System.out.println("IT IS AN ALIEN");
         } else if(!isAlien) {
             System.out.println("it is not an alien!");
         }
+
+	    int topScore = 80;
+	    if (topScore == 100){
+            System.out.println("YOU WIN");
+        } else if (topScore >= 50){
+            System.out.println("SO CLOSE");
+        } else {
+            System.out.println("Try harder next time!");
+        }
+
     }
 }
